2de3851bc2813e968ec5365387d195423acf8c3f
qiukeren <qiuker521@163.com>
2021-05-19 09:51:51 +0800

    move include to init.h

--- init/init.h
+++ init/init.h
@@ -5,10 +5,23 @@
 #ifndef CHUAN_INIT_H
 #define CHUAN_INIT_H
 
+#include <stdio.h>
+#include <signal.h>
+#include <sys/mount.h>
+#include <linux/reboot.h>
+#include <linux/wait.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/reboot.h>
+
+
 static void handler(int signal);
 
 char *trim_space(char *str);
 
 static void execute(char *path);
-
-#endif //CHUAN_INIT_H
+void mountvirtfs();
+#endif				//CHUAN_INIT_H
